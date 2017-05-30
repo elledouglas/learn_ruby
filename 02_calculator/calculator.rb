5327bb6ffffa255b7f0ec5558e2d710de73e5939
Kathleen <kathleenmcclay@gmail.com>
2017-05-30 16:17:28 -0400

    2nd complete

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,15 @@
+def add(integer, integer2)
+  integer + integer2
+end
+
+def subtract(minus, minus2)
+  minus - minus2
+end
+
+def sum(array)
+  sum = 0
+  array.each do |array|
+    sum += array
+end
+return sum 
+end
